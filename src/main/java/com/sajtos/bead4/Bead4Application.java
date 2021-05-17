c627145ed81f0d4d8ec069e17b14d1943d8802af
sajtosM <matthias.sajtos@concircle.com>
2021-05-17 22:02:36 +0200

    🎉Initial commit

---
+++ src/main/java/com/sajtos/bead4/Bead4Application.java
@@ -0,0 +1,13 @@
+package com.sajtos.bead4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bead4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bead4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sajtos/bead4/Bead4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajtos.bead4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bead4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
